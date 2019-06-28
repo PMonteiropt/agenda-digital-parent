b374c2e1889efed53d11e71cf7135aae395d39aa
Pedro <pedromiguel309@hotmail.com>
2019-06-28 18:05:24 +0100

    sub

---
+++ agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/servlets/Hello.java
@@ -0,0 +1,31 @@
+package com.everis.academia.java.agenda.digital.web.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "hello", urlPatterns = "/hello")
+public class Hello extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello Academia Java");
+		out.println("</body");
+		out.println("</html>");
+	}
+
+	
+
+}
