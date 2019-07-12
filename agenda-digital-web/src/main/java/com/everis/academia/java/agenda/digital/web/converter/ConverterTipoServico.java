d5883a280983bd76069bec10eb8e5d38656a6db8
Pedro <pedromiguel309@hotmail.com>
2019-07-12 17:04:18 +0100

    update

---
+++ agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/converter/ConverterTipoServico.java
@@ -0,0 +1,37 @@
+package com.everis.academia.java.agenda.digital.web.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import com.everis.academia.agenda.digital.entity.TipoServico;
+
+@FacesConverter(value = "TipoServico", forClass = TipoServico.class)
+public class ConverterTipoServico implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		
+		TipoServico tiposervico=null;
+		
+		if(value !=null && !value.trim().isEmpty()) {
+			
+			tiposervico = new TipoServico(Short.valueOf(value.trim()));
+		}
+		return tiposervico;
+		
+		
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		
+		if(value instanceof TipoServico) {
+			
+			return String.valueOf(((TipoServico)value).getCodigo());
+		}
+		return (String) value;
+	}
+
+}
